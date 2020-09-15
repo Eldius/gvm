b02076059c33ba0557c7f6cea58fe20c745b5529
eldius <eldiosantos@gmail.com>
2020-09-15 19:32:32 -0300

    Some refactoring...

--- cmd/lsRemote.go
+++ cmd/lsRemote.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/Eldius/go-version-manager/versions"
 	"github.com/spf13/cobra"
@@ -12,17 +11,12 @@ import (
 var lsRemoteCmd = &cobra.Command{
 	Use:   "ls-remote",
 	Short: "List available Go versions",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `List available Go versions.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("lsRemote called")
 		versions := versions.ListAvailableVersions()
 		for _, v := range versions {
-			log.Printf("- %s => %s (%s)\n", v.Name, v.LinuxAmd64, v.Source)
+			fmt.Printf("- %s\n", v.Name)
 		}
 	},
 }
--- config/config.go
+++ config/config.go
@@ -2,18 +2,31 @@ package config
 
 import (
 	"log"
+	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
+)
+
+const (
+	configDir = "~/.gvm"
 )
 
 /*
 Root returns the app config root folder
 */
 func Root() string {
-	cfgDir, err := homedir.Expand("~/.gvm")
+	cfgDir, err := homedir.Expand(configDir)
 	if err != nil {
 		log.Println("Failed to parse config folder")
 		log.Panicln(err.Error())
 	}
 	return cfgDir
+}
+
+func GetWorkspaceDir() string {
+	return filepath.Join(Root(), "workspace")
+}
+
+func GetVersionsDir() string {
+	return filepath.Join(GetWorkspaceDir(), "versions")
 }
