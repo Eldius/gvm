16dd2ccc70c3f9af86af33282dc667d1cde52ac3
Eldio Santos Junior <eldiosantos@gmail.com>
2020-09-18 15:04:36 +0000

    Added more validation to unpack test...

--- installer/installer_test.go
+++ installer/installer_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/Eldius/go-version-manager/versions"
@@ -19,8 +20,7 @@ func init() {
 	if err != nil {
 		log.Panic("Error creating temp dir\n", err.Error())
 	}
-	log.Println("temp dir:", tmpDir)
-	os.Setenv("USER", tmpDir)
+	viper.SetDefault("gvm.cfg.dir", tmpDir)
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
@@ -35,5 +35,24 @@ func TestUnpack(t *testing.T)  {
 	}
 	log.Printf("testing...")
 	unpack("test_data/test.tar.gz", v)
+	wsDir := filepath.Join(tmpDir, "workspace")
+	if fs, err := os.Stat(wsDir); err != nil {
+		t.Errorf("Could not stat workspace folder at '%s'...", wsDir)
+	} else if !fs.IsDir() {
+		t.Errorf("'%s' must be a folder...", wsDir)
+	}
 
+	versionDir := filepath.Join(wsDir, "versions", v.Name)
+	if fs, err := os.Stat(versionDir); err != nil {
+		t.Errorf("Could not stat version folder at '%s'...", versionDir)
+	} else if !fs.IsDir() {
+		t.Errorf("'%s' must be a folder...", versionDir)
+	}
+
+	unpackedFilePath := filepath.Join(versionDir, "bin", "test_file01.txt")
+	if fs, err := os.Stat(unpackedFilePath); err != nil {
+		t.Errorf("Could not stat unpacked file at '%s'...", unpackedFilePath)
+	} else if fs.IsDir() {
+		t.Errorf("Go binary '%s' must be a file...", unpackedFilePath)
+	}
 }
