7c1b00d8c4cf31bda05df8dd0accec019a2d8d6e
eldius <eldiosantos@gmail.com>
2023-02-02 19:45:54 -0300

    Fixing download issue...

--- versions/model.go
+++ versions/model.go
@@ -1,56 +1,52 @@
 package versions
 
 import (
-    "fmt"
-    "io"
-    "net/http"
-    "os"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
 )
 
 /*
 GoVersion golang version representation
 */
 type GoVersion struct {
-    Name       string
-    LinuxAmd64 string
-    Source     string
+	Name       string
+	LinuxAmd64 string
+	Source     string
 }
 
 /*
 Download downloads this version
 */
 func (v *GoVersion) Download() (filePath string, err error) {
-    fmt.Printf("Download package for version %s\n", v.Name)
+	fmt.Printf("Download package for version %s\n", v.Name)
 
-    var resp *http.Response
-    resp, err = http.Get(v.LinuxAmd64)
-    if err != nil {
-        return
-    }
-    defer func() {
-        _ = resp.Body.Close()
-    }()
-    tmpFilePath, err := os.MkdirTemp("", fmt.Sprintf("%s.tar.gz", v.Name))
-    if err != nil {
-        return
-    }
+	var resp *http.Response
+	resp, err = http.Get(v.LinuxAmd64)
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	tmpFilePath, err := os.MkdirTemp("", fmt.Sprintf("%s.tar.gz", v.Name))
+	if err != nil {
+		return
+	}
 
-    tmpFileStats, err := os.Stat(tmpFilePath)
-    if err != nil {
-        return
-    }
+	// Create the file
+	file, err := os.OpenFile(path.Join(tmpFilePath, fmt.Sprintf("%s.tar.gz", v.Name)), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = file.Close()
+	}()
 
-    // Create the file
-    file, err := os.OpenFile(tmpFileStats.Name(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return
-    }
-    defer func() {
-        _ = file.Close()
-    }()
-
-    // Write the body to file
-    _, err = io.Copy(file, resp.Body)
-    filePath = file.Name()
-    return
+	// Write the body to file
+	_, err = io.Copy(file, resp.Body)
+	filePath = file.Name()
+	return
 }
--- versions/model_test.go
+++ versions/model_test.go
@@ -1,48 +1,49 @@
 package versions
 
 import (
-    "log"
-    "os"
-    "testing"
+	"log"
+	"os"
+	"testing"
 
-    "gopkg.in/h2non/gock.v1"
+	"gopkg.in/h2non/gock.v1"
 )
 
 const (
-    versionDownload = "https://download.com/dl/go1.14.tar.gz"
+	versionDownload = "https://download.com/dl/go1.14.tar.gz"
 )
 
 func TestDownload(t *testing.T) {
-    defer gock.Off()
+	defer gock.Off()
 
-    v := GoVersion{
-        LinuxAmd64: versionDownload,
-    }
+	v := GoVersion{
+		Name:       "go1.14",
+		LinuxAmd64: versionDownload,
+	}
 
-    gock.New(versionDownload).
-        Get("/").
-        Reply(200).
-        File("test_data/download.html")
+	gock.New(versionDownload).
+		Get("/").
+		Reply(200).
+		File("test_data/download.html")
 
-    f, err := v.Download()
-    if err != nil {
-        t.Error("Failed to download file:", err)
-        t.FailNow()
-    }
+	f, err := v.Download()
+	if err != nil {
+		t.Error("Failed to download file:", err)
+		t.FailNow()
+	}
 
-    log.Printf("downloaded file: '%s'", f)
-    if f == "" {
-        t.Error("File should be not nil:", err)
-        t.FailNow()
-    }
+	log.Printf("downloaded file: '%s'", f)
+	if f == "" {
+		t.Error("File should be not nil:", err)
+		t.FailNow()
+	}
 
-    i, err := os.Stat(f)
-    if err != nil {
-        t.Error("Failed validate file state:", err)
-        t.FailNow()
-    }
-    if i.IsDir() {
-        t.Error("Returned file path must be a file (but was a directory):", err)
-        t.FailNow()
-    }
+	i, err := os.Stat(f)
+	if err != nil {
+		t.Error("Failed validate file state:", err)
+		t.FailNow()
+	}
+	if i.IsDir() {
+		t.Error("Returned file path must be a file (but was a directory):", err)
+		t.FailNow()
+	}
 }
